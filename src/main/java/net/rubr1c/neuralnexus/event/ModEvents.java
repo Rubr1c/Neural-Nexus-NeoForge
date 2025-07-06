344acd5a568efc088f13dfba68b2a88e567e5486
Ali Zaghloul <alizaghloul64@gmail.com>
2025-07-06 17:40:04 +0300

    refactor(LearnerSword): streamline event handling and NBT management
    - Moved event handling logic for learning and drops into the LearnerSwordItem class.
    - Simplified the ModEvents class by delegating responsibilities to the LearnerSwordItem methods.
    - Improved readability and maintainability of the code by reducing complexity in event handling.

--- src/main/java/net/rubr1c/neuralnexus/event/ModEvents.java
+++ src/main/java/net/rubr1c/neuralnexus/event/ModEvents.java
@@ -1,41 +1,21 @@
 package net.rubr1c.neuralnexus.event;
 
-import net.minecraft.core.HolderLookup;
-import net.minecraft.core.registries.BuiltInRegistries;
-import net.minecraft.nbt.CompoundTag;
-import net.minecraft.network.chat.Component;
-import net.minecraft.resources.ResourceLocation;
-import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.damagesource.DamageSource;
-import net.minecraft.world.entity.Entity;
-import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.animal.Pig;
 import net.minecraft.world.entity.boss.wither.WitherBoss;
-import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.storage.loot.LootParams;
-import net.minecraft.world.level.storage.loot.LootTable;
-import net.minecraft.world.level.storage.loot.parameters.LootContextParamSets;
-import net.minecraft.world.level.storage.loot.parameters.LootContextParams;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.event.entity.living.LivingDeathEvent;
 import net.neoforged.neoforge.event.entity.living.LivingDropsEvent;
 import net.neoforged.neoforge.event.entity.player.PlayerInteractEvent;
-import net.neoforged.neoforge.items.ItemStackHandler;
 import net.rubr1c.neuralnexus.NeuralNexus;
-import net.rubr1c.neuralnexus.codec.LearnerModelData;
 import net.rubr1c.neuralnexus.component.ModDataComponents;
 import net.rubr1c.neuralnexus.item.ModItems;
-import net.rubr1c.neuralnexus.item.custom.LearnerModelItem;
 import net.rubr1c.neuralnexus.item.custom.LearnerSwordItem;
-import net.rubr1c.neuralnexus.screen.custom.LearnerSwordMenu;
-
-import java.util.List;
-import java.util.Random;
 
 @EventBusSubscriber(modid = NeuralNexus.MODID, bus = EventBusSubscriber.Bus.GAME)
 public class ModEvents {
@@ -63,35 +43,7 @@ public class ModEvents {
             if (stack.getItem() instanceof LearnerSwordItem) {
                 Boolean isLearning = stack.get(ModDataComponents.BOOL);
                 if (isLearning != null && isLearning) {
-                    var comp = stack.get(ModDataComponents.NBT);
-                    if (comp == null) return;
-
-                    HolderLookup.Provider provider = player.level().registryAccess();
-                    ItemStackHandler handler = new ItemStackHandler(9);
-
-                    CompoundTag swordInv = comp.getCompound(LearnerSwordItem.NBT_KEY);
-                    handler.deserializeNBT(provider, swordInv);
-
-
-                    for (int i = 0; i < 9; i++) {
-                        ItemStack curr = handler.getStackInSlot(i);
-                        if (!(curr.getItem() instanceof LearnerModelItem)) continue;   // skip empties
-
-                        ResourceLocation entity = curr.get(ModDataComponents.LIVING_ENTITY);
-                        if (entity == null) continue;
-
-                        if (dead.getType().equals(BuiltInRegistries.ENTITY_TYPE.get(entity))) {
-                            int acc = curr.getOrDefault(ModDataComponents.PERCENT, 0);
-                            ItemStack updated = curr.copy();
-                            updated.set(ModDataComponents.PERCENT, Math.min(acc + 1, 100));
-                            handler.setStackInSlot(i, updated);
-
-                            comp.put(LearnerSwordItem.NBT_KEY, handler.serializeNBT(provider));
-                            stack.set(ModDataComponents.NBT, comp);
-                            break;
-                        }
-
-                    }
+                    LearnerSwordItem.handleLearn(player, stack, dead);
                 }
             }
         }
@@ -100,19 +52,10 @@ public class ModEvents {
     @SubscribeEvent
     public static void onLeftClickWithLearnerSword(PlayerInteractEvent.LeftClickBlock event) {
         ItemStack stack = event.getItemStack();
+        Player player = event.getEntity();
 
         if (stack.getItem() instanceof LearnerSwordItem) {
-            Boolean state = stack.get(ModDataComponents.BOOL);
-
-            Player player = event.getEntity();
-            if (state == null) {
-                stack.set(ModDataComponents.BOOL, true);
-                player.displayClientMessage(Component.literal("Simulating"), true);
-
-            } else {
-                stack.set(ModDataComponents.BOOL, !state);
-                player.displayClientMessage(Component.literal(state ? "Simulating" : "Learning"), true);
-            }
+            LearnerSwordItem.toggleMode(player, stack);
         }
     }
 
@@ -130,37 +73,7 @@ public class ModEvents {
         Boolean isLearning = stack.get(ModDataComponents.BOOL);
         if (isLearning != null && isLearning) return;
 
-        LearnerModelData data = stack.get(ModDataComponents.MODEL);
-        if (data == null) return;
-
-        Random random = new Random();
-
-        if (random.nextInt(100) > data.accuracy()) {
-            return;
-        }
-
-        var target = BuiltInRegistries.ENTITY_TYPE.get(data.entity());
-        var lootTable = target.getDefaultLootTable();
-        LootTable drops = level.getServer().reloadableRegistries().getLootTable(lootTable);
-
-        LivingEntity entity = event.getEntity();
-        event.getDrops().clear();
-
-        LootParams.Builder builder = new LootParams.Builder((ServerLevel)level)
-                .withParameter(LootContextParams.THIS_ENTITY, event.getEntity())
-                .withParameter(LootContextParams.ORIGIN, event.getEntity().position());
-        LootParams params = builder.create(LootContextParamSets.EMPTY);
-
-        List<ItemStack> rolls = drops.getRandomItems(params);
-        for (ItemStack drop : rolls) {
-            event.getDrops().add(new ItemEntity(
-                    level,
-                    entity.getX(),
-                    entity.getY(),
-                    entity.getZ(),
-                    drop
-            ));
-        }
+        LearnerSwordItem.handleDrops(level, stack, event.getEntity(), event.getDrops());
 
     }
 
--- src/main/java/net/rubr1c/neuralnexus/item/custom/LearnerSwordItem.java
+++ src/main/java/net/rubr1c/neuralnexus/item/custom/LearnerSwordItem.java
@@ -2,27 +2,40 @@ package net.rubr1c.neuralnexus.item.custom;
 
 import io.netty.buffer.Unpooled;
 import net.minecraft.core.HolderLookup;
+import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.SimpleMenuProvider;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.SwordItem;
 import net.minecraft.world.item.Tiers;
 import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.storage.loot.LootParams;
+import net.minecraft.world.level.storage.loot.LootTable;
+import net.minecraft.world.level.storage.loot.parameters.LootContextParamSets;
+import net.minecraft.world.level.storage.loot.parameters.LootContextParams;
 import net.neoforged.neoforge.items.ItemStackHandler;
 import net.rubr1c.neuralnexus.codec.LearnerModelData;
 import net.rubr1c.neuralnexus.component.ModDataComponents;
 import net.rubr1c.neuralnexus.item.ModItems;
 import net.rubr1c.neuralnexus.screen.custom.LearnerSwordMenu;
 
+import java.util.Collection;
 import java.util.List;
+import java.util.Random;
 
 public class LearnerSwordItem extends SwordItem {
     public static final String NBT_KEY = "LearnerSwordInv";
@@ -99,6 +112,93 @@ public class LearnerSwordItem extends SwordItem {
             handler.deserializeNBT(provider, swordInv);
         } else {
             held.set(ModDataComponents.NBT, new CompoundTag());
+        }
+    }
+
+    public static void handleLearn(Player player,
+                                   ItemStack stack,
+                                   LivingEntity dead) {
+        CompoundTag comp = stack.get(ModDataComponents.NBT);
+        if (comp == null) return;
+
+        HolderLookup.Provider provider = player.level().registryAccess();
+        ItemStackHandler handler = new ItemStackHandler(9);
+
+        CompoundTag swordInv = comp.getCompound(LearnerSwordItem.NBT_KEY);
+        handler.deserializeNBT(provider, swordInv);
+
+
+        for (int i = 0; i < 9; i++) {
+            ItemStack curr = handler.getStackInSlot(i);
+            if (!(curr.getItem() instanceof LearnerModelItem)) continue;   // skip empties
+
+            ResourceLocation entity = curr.get(ModDataComponents.LIVING_ENTITY);
+            if (entity == null) continue;
+
+            if (dead.getType().equals(BuiltInRegistries.ENTITY_TYPE.get(entity))) {
+                int acc = curr.getOrDefault(ModDataComponents.PERCENT, 0);
+                ItemStack updated = curr.copy();
+                updated.set(ModDataComponents.PERCENT, Math.min(acc + 1, 100));
+                handler.setStackInSlot(i, updated);
+
+                comp.put(LearnerSwordItem.NBT_KEY, handler.serializeNBT(provider));
+                stack.set(ModDataComponents.NBT, comp);
+                break;
+            }
+
+        }
+    }
+
+    public static void toggleMode(Player player, ItemStack stack) {
+        Boolean state = stack.get(ModDataComponents.BOOL);
+
+        if (state == null) {
+            stack.set(ModDataComponents.BOOL, true);
+            player.displayClientMessage(Component.literal("Simulating"), true);
+
+        } else {
+            stack.set(ModDataComponents.BOOL, !state);
+            player.displayClientMessage(Component.literal(state ? "Simulating" : "Learning"), true);
+        }
+    }
+
+    public static void handleDrops(Level level,
+                                   ItemStack stack,
+                                   LivingEntity killed,
+                                   Collection<ItemEntity> killedDrop) {
+        LearnerModelData data = stack.get(ModDataComponents.MODEL);
+        if (data == null) return;
+
+        Random random = new Random();
+
+        if (random.nextInt(100) > data.accuracy()) {
+            return;
+        }
+
+        EntityType<?> target = BuiltInRegistries.ENTITY_TYPE.get(data.entity());
+        ResourceKey<LootTable> lootTable = target.getDefaultLootTable();
+
+        MinecraftServer server = level.getServer();
+        if (server == null) return;
+
+        LootTable drops = server.reloadableRegistries().getLootTable(lootTable);
+
+        killedDrop.clear();
+
+        LootParams.Builder builder = new LootParams.Builder((ServerLevel)level)
+                .withParameter(LootContextParams.THIS_ENTITY, killed)
+                .withParameter(LootContextParams.ORIGIN, killed.position());
+        LootParams params = builder.create(LootContextParamSets.EMPTY);
+
+        List<ItemStack> rolls = drops.getRandomItems(params);
+        for (ItemStack drop : rolls) {
+            killedDrop.add(new ItemEntity(
+                    level,
+                    killed.getX(),
+                    killed.getY(),
+                    killed.getZ(),
+                    drop
+            ));
         }
     }
 
